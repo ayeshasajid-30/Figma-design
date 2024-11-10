e28eee58a1e304e064929c33fc688aa71401f296
ayeshasajid-30 <160410976+ayeshasajid-30@users.noreply.github.com>
2024-11-11 01:10:53 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  // Enable additional Next.js configurations here if needed
+}
+
